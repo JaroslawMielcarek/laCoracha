c25b71f88df7e2e02d6ebd2e76f248acde0aa28e
Yarek <jaroslaw@mielcarek.co.uk>
2022-09-29 12:59:14 +0200

    hotfix

--- src/services/middleware/axio.js
+++ src/services/middleware/axio.js
@@ -4,7 +4,7 @@ import store from '../../store/index'
 import router from '../../router/index'
 import { setNotification } from '../util/universal'
 
-const API_URL = `${process.env.HOSTNAME}/api/`
+const API_URL = `${process.env.BACKEND}/api/`
 
 export default function axio (url, data, headers = undefined) {
   return axios
