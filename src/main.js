9284aabf6e00721417626e987ca7c8cf661108a6
Yarek <jaroslaw@mielcarek.co.uk>
2022-11-04 12:54:32 +0100

    csp test

--- src/main.js
+++ src/main.js
@@ -7,4 +7,11 @@ import './styles.css'
 createApp(App)
   .use(router)
   .use(store)
+  .use(function (req, res, next) {
+    res.setHeader(
+      'Content-Security-Policy', "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+    );
+    
+    next();
+  })
   .mount('#app')
