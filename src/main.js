a205e80c66dcc8fbfe083499810f2aab86d1fd83
Yarek <jaroslaw@mielcarek.co.uk>
2022-11-04 12:58:34 +0100

    remove test

--- src/main.js
+++ src/main.js
@@ -7,11 +7,4 @@ import './styles.css'
 createApp(App)
   .use(router)
   .use(store)
-  .use(function (req, res, next) {
-    res.setHeader(
-      'Content-Security-Policy', "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
-    );
-    
-    next();
-  })
   .mount('#app')
