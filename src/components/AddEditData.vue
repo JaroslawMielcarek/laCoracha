22b0f2332f04740c52dec625cbf276e22884bce1
Yarek <jaroslaw@mielcarek.co.uk>
2023-01-19 19:11:55 +0100

    add universal AddEdit component and refactor

---
+++ src/components/AddEditData.vue
@@ -0,0 +1,24 @@
+<template>
+  <form class='add_edit' @submit.prevent="emit('submitForm')">
+    <h4>{{ isEditing ? 'Actualizar' : 'Añadir nueva' }} {{ category }}</h4>
+    <slot>Loading..</slot>
+    <div class='row flex-row'>
+      <button type='submit' class='btn color'>{{ isEditing ? 'Actualizar' : 'Añadir' }}</button>
+      <p class='btn text' @click="emit('closeForm') ">Cierra</p>
+    </div>
+  </form>
+</template>
+
+<script setup>
+
+const emit = defineEmits(['closeForm', 'submitForm'])
+const props = defineProps({
+  isEditing: {type: Boolean, default: false},
+  category: {type: String, default: 'unknown'}
+})
+    
+</script>
+
+<style lang="scss" scoped>
+@import '@/colors.scss';
+</style>
--- src/components/moderator/practice/PracticeManager.vue
+++ src/components/moderator/practice/PracticeManager.vue
@@ -1,73 +1,154 @@
 <template>
   <div class='container full-width'>
-    <h3>Quedadas</h3>
-    <p class='extra__message'>Administrar el calendario de quedadas</p>
-    <div>
-      <span>Mostrar: </span>
-      <CustomSelectInput v-model:value="showBy" :options="['Todo', 'Semana', 'Mes', 'Temporada']" placeholder="Eligue periodo" />
-    </div>
-    <div class='grid row'>
+    <h3>Jugadores</h3>
+    <p class='extra__message'>Administrar jugadores del club</p>
+    <div class='grid'>
       <div class='list-head'>
         <p class='column'></p>
-        <p class='column'>Día de la semana</p>
-        <p class='column'>Fecha</p>
-        <p class='column'>Hora</p>
-        <p class='column'>Jugadores</p>
+        <p class='column sort' @click="sortBy = 'memberID'">ID</p>
+        <p class='column sort' @click="sortBy = 'name'">Nick</p>
+        <p class='column'>No.</p>
+        <p class='column'>Género</p>
+        <p class='column sort' @click="sortBy = 'team'">Equipo</p>
         <p class='column'></p>
       </div>
-      <div class='list'>
-        <div class='list-row' v-if="!practices.length">
-          <p class="no-data">No hay quedadas para mostrar</p>
+      <div class='list large'>
+        <div class='list-row' v-if="!players.length">
+          <p class="no-data">No hay jugadores para mostrar</p>
         </div>
-        <div class='list-row' v-for="practice in practices" :key="practice">
-          <p class='column'><button class='btn color' @click="setState(practice, true, true)">Editar</button></p>
-          <p class='column'>{{ getDayOfWeek(practice.dateTime.date) }}</p>
-          <p class='column'>{{ isoDateToDayMonthYear(practice.dateTime.date) }}</p>
-          <p class='column'>{{ practice.dateTime.time }}</p>
-          <p :class="['column',{ overLimit: practice.playersSubscribed > practice.playersLimit }]"><b>{{ practice.playersSubscribed }}</b>/{{ practice.playersLimit }}</p>
-          <p class='column'><button class='btn danger' @click="removeElement('Practice', practice)">x</button></p>
+        <div class='list-row' v-for="player in players" :key="player">
+          <div class='action column'><button class='btn color' @click="setState(player, true)">Editar</button></div>
+          <p class='column id'>{{ player.memberID || 'no data' }}</p>
+          <p class='column name'>{{ player.nick ? player.nick.value : '' }}</p>
+          <p class='column number'>{{ player.number? player.number.value : '' }}</p>
+          <p class='column gender'>{{ player.isFemale ? 'Mujer' : 'Hombre' }}</p>
+          <p class='column team'>{{ player.team }}</p>
+          <div class='action column'><button class='btn danger' @click="removeElement('Player', player)">x</button></div>
         </div>
       </div>
     </div>
-    <button v-if="!value" class='btn white' @click="value = store.getters.getDefaultPractice">Agregar Quedada</button>
-    <AddEditPractice v-else :value="value" :isEditing="isEditing" @submitForm="(acction, value) => submitForm(acction, value, setState(undefined))" @clearForm="setState(undefined)"/>
+    <AddEditData v-if="choosedValue" category="Jugador" :isEditing="isEditing" @submitForm="submitForm( isEditing ? 'updatePlayer' : 'addPlayer', setInTeamPerformancePercents(choosedValue.value), setState(undefined))" @closeForm="setState(undefined)">
+      <div class='row grid-row'>
+        <div class='column'>
+          <h5>Lo esencial</h5>
+          <div class='row'>
+            <CustomInput v-model:value="choosedValue.memberID" placeholder='ID' :required='true' @update:value="checkIfExist"/>
+            <CustomInput v-model:value="choosedValue.nick.value" placeholder='Nick' />
+            <CustomInput v-model:value="choosedValue.number.value" pattern='^[0-9]*$' hint='Solo números permitidos' placeholder='Numero'/>
+          </div>
+          <div class='row toggle'>
+            <label class='mujer label__inline'>Mujer:</label>
+            <ToggleSlider  :checked="choosedValue.isFemale" @toggled="choosedValue.isFemale = !choosedValue.isFemale"/>
+          </div>
+          <p class='team'>Equipo: <span class='value'>{{ choosedValue.team ? choosedValue.team : "No pertenece a ninguno" }}</span></p>
+        </div>
+        <div class='column'>
+          <PlayerPractices v-if="choosedValue.practices" v-model:value="choosedValue.practices" />
+        </div>
+      </div>
+      <ImagePrevWithRemoveVue v-if="choosedValue.photo && choosedValue.photo.value" :image="file" @removeLogo="choosedValue.photo.value=undefined"/>
+      <CustomUploadFile v-else text="Photo" :file="file" :sizeLimit="200000" @fileChoosed="(val) => choosedValue.photo.value = val"/>
+
+      <PlayerPerformance v-if="choosedValue.team" v-model:value="choosedValue.inTeamPerformance" />
+    </AddEditData>
   </div>
 </template>
 
 <script setup>
 import { useStore } from 'vuex'
 import { ref, computed, onMounted } from 'vue'
-import AddEditPractice from '@/components/moderator/practice/AddEditPractice.vue'
-import CustomSelectInput from '@/components/CustomSelectInput.vue'
-import { isoDateToDayMonthYear, getDayOfWeek } from '@/services/util/time.js'
-import { setNotification, submitForm, removeElement} from '@/services/util/universal.js'
+import AddEditData from '../../AddEditData.vue'
+import ImagePrevWithRemoveVue from '@/components/ImagePrevWithRemove.vue'
+import CustomUploadFile from '@/components/CustomUploadFile.vue'
+import PlayerPerformance from '@/components/moderator/player/PlayerPerformance.vue'
+import PlayerPractices from '@/components/moderator/player/PlayerPractices.vue'
+import CustomInput from '@/components/CustomInput.vue'
+import ToggleSlider from '@/components/ToggleSlider.vue'
+import { sortListOfObjectsBy } from '@/services/util/object.js'
+import { setNotification, submitForm, removeElement } from '@/services/util/universal.js'
 
 const isEditing = ref(false)
-const value = ref(undefined)
-const showBy = ref('Todo')
-
+const choosedValue = ref(undefined)
+const sortBy = ref('memberID')
 const store = useStore()
 
-onMounted( () => { store.dispatch('fetchPractices') })
+onMounted( () => { store.dispatch('fetchPlayers')})
 
-const practices = computed( () => {
-  if (showBy.value === 'Semana') return store.getters.getPracticesOf('week')
-  if (showBy.value === 'Mes') return store.getters.getPracticesOf('month')
-  if (showBy.value === 'Temporada') return store.getters.getPracticesOf('season')
-  return store.getters.getPractices
+const players = computed( () => {
+  const list = store.getters.getPlayers
+  if (sortBy.value === 'memberID') return sortListOfObjectsBy(list, 'memberID', false)
+  if (sortBy.value === 'team') return sortListOfObjectsBy(list, 'team', false)
+  if (sortBy.value === 'name') return sortListOfObjectsBy(list, 'nick', false)
+  return list
 })
 
 function setState(val, isEdit = false) {
-  value.value = val
+  !val
+    ? choosedValue.value = undefined
+    : choosedValue.value = JSON.parse(JSON.stringify(val)) //deep copy
   isEditing.value = isEdit
 }
 
+const file = computed( () =>  choosedValue.value.photo && choosedValue.value.photo.value ? choosedValue.value.photo.value : {} )
+
+function setInTeamPerformancePercents (player) {
+  // We take old player object and we update current player object
+  // prevPercent with old percent
+  for (const [name, prop] of Object.entries(player.inTeamPerformance)) {
+    choosedValue.value.prevPercent = choosedValue.value.inTeamPerformance[name].percent
+  }
+  return player
+}
+function checkIfExist (value) {
+  const existingValues = store.getters.getPlayersIDs
+  const result = existingValues.find(t => t.toString().toLowerCase() === value.toString().toLowerCase())
+  if (result) {
+    alert('El ID ya existe. Por favor, edite en su lugar')
+    choosedValue.value.id = ''
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 @import '@/colors.scss';
 .list-head,
 .list-row {
-  grid-template-columns: 60px repeat(3, 1fr) 60px 50px;
+  // max 375px 220 = 150 / 2 = 75
+  grid-template-columns: 60px 30px minmax(60px, 1fr) 30px 60px minmax(80px, 1fr) 50px;
+
+  .id,
+  .name,
+  .number,
+  .gender,
+  .team {
+    margin-bottom: .1em;
+  }
+  .number,
+  .gender{
+    font-weight: 300;
+  }
+}
+.team {
+  .value {
+    color: $blueDark;
+  }
+}
+.column {
+  display: inline-block;
+  margin-right: 20px;
+  &:last-of-type{
+    margin-right: 0;
+  }
+}
+.grid-row {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.mujer {
+  margin-right: 1ch;
+}
+.toggle {
+  display: flex;
+  align-items: center;
 }
 </style>
