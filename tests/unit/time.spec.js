23a6944f23ef1e1e73d8f4081dcbca5ba5a72b3b
JaroslawMielcarek <83643906+JaroslawMielcarek@users.noreply.github.com>
2023-01-19 15:01:48 +0100

    Remove unnecessary files

--- tests/unit/time.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import { getDayOfWeek } from '@/services/util/time.js'
-
-describe('time util function test', () => {
-
-  test('get day of the week', () => {
-    expect(getDayOfWeek(new Date())).toBe('Sabado')
-  })
-})
