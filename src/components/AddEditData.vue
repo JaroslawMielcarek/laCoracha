fab79722eb97f0ba85d75c2f97f2ab44bc9452d8
Yarek <jaroslaw@mielcarek.co.uk>
2023-02-07 18:01:15 +0100

    style corrections

--- src/components/AddEditData.vue
+++ src/components/AddEditData.vue
@@ -1,6 +1,6 @@
 <template>
   <div class='modal-wrapper' @click="emit('closeForm')">
-    <form class='add_edit' @submit.prevent="emit('submitForm')" @click.stop="">
+    <form class='modal' @submit.prevent="emit('submitForm')" @click.stop="">
       <h4>{{ isEditing ? 'Actualizar' : 'Añadir nueva' }} {{ category }}</h4>
       <slot>Loading..</slot>
       <div class='row flex-row'>
@@ -26,38 +26,7 @@ const props = defineProps({
 .row {
   margin-top: 2rem;
 }
-.modal-wrapper{
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  position: absolute;
-  top: 0;
-  left: 0;
-  min-width: 100%;
-  min-height: 100%;
-  background-color: rgba($decorativa-neutral-dark, .4);
-  overflow-y: scroll;
-  padding: 0 0 44px;
-  z-index: 10;
-}
-
-.add_edit {
-  border: 1px solid $white;
-  border-radius: 4px;
-  background-color: $white;
-  padding: 1rem;
-  margin: 20px auto;
-  box-shadow: 0 4px 20px rgba($blueDark, .2);
-  min-width: 360px;
-  max-width: min-content;
-}
 
 @media screen and (min-width: 740px) {
-  .modal-wrapper {
-    justify-content: center;
-  }
-  .add_edit {
-    max-width: 800px;
-  }
 }
 </style>
--- src/views/Moderator.vue
+++ src/views/Moderator.vue
@@ -3,27 +3,27 @@
     <h1>Moderator Area</h1>
     <Tabs>
       <Tab title="Calendario">
-        <p class='extra-message'>Administrar el calendario de partidos</p>
+        <p class='extra-message row'>Administrar el calendario de partidos</p>
         <p v-if="fetchError.calendar" class="no-data">{{ fetchError.calendar }}</p>
         <CalendarManager v-else/>
       </Tab>
       <Tab title="Equipos">
-        <p class='extra-message'>Administrar equipos de club</p>
+        <p class='extra-message row'>Administrar equipos de club</p>
         <p v-if="fetchError.teams" class="no-data">{{ fetchError.teams }}</p>
         <TeamsManager v-else/>
       </Tab>
       <Tab title="Jugadores"> 
-        <p class='extra-message'>Administrar jugadores del club</p>
+        <p class='extra-message row'>Administrar jugadores del club</p>
         <p v-if="fetchError.players" class="no-data">{{ fetchError.players }}</p>
         <PlayersManager v-else/>
       </Tab>
       <Tab title="Quedadas">
-        <p class='extra-message'>Administrar el calendario de quedadas</p>
+        <p class='extra-message row'>Administrar el calendario de quedadas</p>
         <p v-if="fetchError.practices" class="no-data">{{ fetchError.practices }}</p>
         <PracticeManager v-else/>
       </Tab>
       <Tab title="Inscriptiones">
-        <p class='extra-message'>Administrar inscripciones al club</p>
+        <p class='extra-message row'>Administrar inscripciones al club</p>
         <p v-if="fetchError.enrolled" class="no-data">{{ fetchError.enrolled }}</p>
         <EnrollmentManager v-else/>
       </Tab>
