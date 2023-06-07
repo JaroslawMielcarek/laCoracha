a0a7ce3acd7c2dcea7f3000fb2294b9b3895f7ae
Yarek <jaroslaw@mielcarek.co.uk>
2023-06-07 20:05:42 +0200

    refactor to ts

--- src/views/Join.vue
+++ src/views/Join.vue
@@ -26,11 +26,11 @@
       </div>
       <fieldset class='inline-group row'>
         <legend class='extra-message'>Genero</legend>
-        <label :class="['btn', {female: gender === 'female'}]">
+        <label :class="['btn', {female: getSelectedGender() === 'female'}]">
           <input v-model="candidate.gender" value='female' type='radio' name='genero' />
           F
         </label>
-        <label :class="['btn', {male: gender === 'male'}]">
+        <label :class="['btn', {male: getSelectedGender() === 'male'}]">
           <input v-model="candidate.gender" value='male' type='radio' name='genero' />
           M
         </label>
@@ -38,13 +38,12 @@
       <fieldset class='inline-group row' v-if="candidate.gender">
         <legend class='extra-message'>Edad</legend>
         <CustomNumberInput v-if="!categories.length" v-model:value="candidate.category" :min="6" :max="65"/>
-        
+
         <template v-else v-for="category in categories" >
-          <label :class="['btn', {[gender]: candidate.category === category.name}]" 
-            v-if="Object.hasOwn(category, gender) && category[gender].currentEnrolled < category[gender].maxPlaces">
+          <label :class="['btn', {[getSelectedGender()]: candidate.category === category.name}]">
             <input v-model="candidate.category" :value="category.name" type="radio" name="category" />
-          <p>{{ category[gender].minAge }} {{ (category[gender].maxAge !== category[gender].minAge) ? ` - ${category[gender].maxAge}` : null }}</p>
-         <p class="extra-message">{{  category.name }}</p>
+            <p>{{ getAgeRange(category[getSelectedGender()]) }}</p>
+            <p class="extra-message">{{  category.name }}</p>
           </label>
         </template>
       </fieldset>
@@ -52,86 +51,88 @@
         <label class='extra-message'>Jugué con este club antes:</label>
         <ToggleSlider  :checked="candidate.wasWithUsBefore" @toggled="candidate.wasWithUsBefore = !candidate.wasWithUsBefore"/>
       </div>
-      <p v-for='err in error' class='error' :key="error">{{ err }}</p>
-      <p v-if="fetchError" class='error' >{{ fetchError }}</p>
+      <p v-for='err in error' class='error'>{{ err }}</p>
       <button class='submit btn color full-width' type='submit'>Unirse</button>
     </form>
-    <div v-if="enrollmentResult" class='modal-wrapper' @click="'dsdds'">
+    <div v-if="enrollmentResult.title" class='modal-wrapper' @click="'dsdds'">
       <div class='modal enrollment-result'>
         <h4>{{ enrollmentResult.title }}</h4>
-        <p class='row'>{{ enrollmentResult.message }}</p>
+        <p class='row'>{{ enrollmentResult["message"] }}</p>
         <!-- icon -->
         <div class='row flex-row'>
           <router-link class="btn color" to='/'>
             Regresar a la página principal
           </router-link>
-          <p class='btn text' @click.prevent="reset">Suscríbete a alguien más</p>
+          <p class='btn text' @click.prevent="Object.assign(enrollmentResult, initialEnrollState)">Suscríbete a alguien más</p>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import CustomInput from '@/components/CustomInput.vue'
 import ToggleSlider from '@/components/ToggleSlider.vue'
 import CustomNumberInput from '@/components/CustomNumberInput.vue'
 import PublicService from '@/services/public.service.js'
-import { ref, onMounted, watch, computed} from 'vue'
-import { setNotification } from '@/services/util/universal.js'
+import { ref, reactive, onMounted, watch, computed} from 'vue'
+import { setNotification, setFirstCapital } from '@/services/util/universal.js'
 
-const emit = defineEmits(['submitForm'])
-const DEFAULT = {
-  name: undefined,
-  phoneNumber: undefined,
-  gender: undefined,
-  category: undefined,
-  wasWithUsBefore: false,
+interface Gender {
+    maxPlaces: number,
+    currentEnrolled: number,
+    minAge: number,
+    maxAge: number
+  }
+interface Category {
+  name: string,
+  maxAge: string,
+  female: Gender,
+  male: Gender
 }
+type GenderOptions = "female" | "male"
 
-const candidate = ref({...DEFAULT})
+const initialCandidateState = { name: '', phoneNumber: '', gender: '', category: '', wasWithUsBefore: false }
+const initialEnrollState = { title: '', message: ''}
 
-const enrollmentResult = ref(undefined)
-const error = ref([])
-const fetchError = ref(undefined)
-const categoryList = ref([])
-const gender = computed( () => candidate.value.gender)
+const candidate = reactive({...initialCandidateState})
+const enrollmentResult = reactive({...initialEnrollState})
+
+const error = ref<string[]>([])
+const categoryList = reactive<Category[]>([])
+
 const categories = computed( () => {
-  if (!gender.value || !categoryList.value.length) return []
-
-  return categoryList.value.filter( c => Object.hasOwn(c, gender.value) && c[gender.value].currentEnrolled < c[gender.value].maxPlaces )
+  if (!categoryList.length) return categoryList
+  return categoryList.filter( c => Object.hasOwn(c, getSelectedGender()) && c[getSelectedGender()].currentEnrolled < c[getSelectedGender()].maxPlaces )
 })
 
 onMounted( () => {
   PublicService.getAll('Enrolled')
-    .then(response => {
-      categoryList.value = response
-    })
-    .catch(err => {
-      fetchError.value = 'Hay un problema para conectarse a la base de datos'
-    })
+    .then(response => categoryList.push(...response) )
+    .catch(() => error.value.push('Hay un problema al conectarse a la base de datos.\nSin embargo, aún puede enviarnos una solicitud.') )
 })
 
-watch( () => candidate.value.gender, () => delete candidate.value.category )
+watch( () => candidate.gender, () => candidate.category = '' )
+function getSelectedGender () { return candidate.gender as GenderOptions }
+function getAgeRange (obj: Gender): string {return obj.minAge + (obj.maxAge !== obj.minAge ? (' - ' + obj.maxAge) : '') }
 
-function checkForm () {
+function checkForm (): boolean {
   error.value = []
 
-  if (!candidate.value.name) error.value.push('Nombre requerido')
-  if (!candidate.value.phoneNumber) error.value.push('Necesitamos numero para contactarte')
-  if (!candidate.value.category) error.value.push('Edad requerida')
+  if (!candidate.name) return !!error.value.push('Nombre requerido')
+  if (!candidate.phoneNumber) return !!error.value.push('Necesitamos numero para contactarte')
+  if (!candidate.gender) return !!error.value.push('Genero requerido')
+  if (!candidate.category) return !!error.value.push('Edad requerida')
 
-  return error.value.length
+  return !!error.value.length
 }
 
-function sendEnrollmentData () {
-  if (checkForm()) return null
-  if (!!fetchError.value || !categories.value.length) return sendMessage()
+function sendEnrollmentData (): void {
+  if (checkForm()) return
+  if (!categories.value.length) return sendMessage()
 
-  PublicService.sendJoinRequest(candidate.value)
-    .then(response => {
-      enrollmentResult.value = response
-    })
+  PublicService.sendJoinRequest(candidate)
+    .then(response => Object.assign(enrollmentResult, response))
     .catch(error => {
       setNotification({
         name: 'Enrolled display',
@@ -140,32 +141,16 @@ function sendEnrollmentData () {
       })
     })
 
-    candidate.value = {...DEFAULT}
+  Object.assign(candidate, initialCandidateState)
 }
 
 function sendMessage () {
-  let text = '*name* _(gender con age años)_ quiere unirse al club. *withUs* y su numero de contacto es *phoneNumber*'
+  let text = `*${setFirstCapital(candidate.name)}* _(${setFirstCapital(candidate.gender)} con ${candidate.category} años)_ quiere unirse al club.
+   *${candidate.wasWithUsBefore ? `Estaba con nosotros antes` : 'No estaba con nosotros antes'}* y su numero de contacto es *${candidate.phoneNumber}*`
 
-  const mapObj = {
-    name: candidate.value.name[0].toUpperCase() + candidate.value.name.slice(1),
-    age: candidate.value.category,
-    gender: candidate.value.gender[0].toUpperCase() + candidate.value.gender.slice(1),
-    phoneNumber: candidate.value.phoneNumber,
-    withUs: candidate.value.wasWithUsBefore ? `Estaba con nosotros antes` : 'No estaba con nosotros antes'
-  }
-  text = text.replace(
-    /name|age|gender|phoneNumber|withUs/gi,
-    function (matched) {
-      return mapObj[matched]
-    }
-  )
   window.location.href = `https://wa.me/393497492300?text=${text}`
 }
 
-function reset () {
-  candidate.value = {...DEFAULT}
-  enrollmentResult.value = null
-}
 </script>
 
 <style lang="scss" scoped>
